49aa1edf25eff202dc2fe4e0a84ea723ded3bc1b
Chohee Kim <kch5559@gmail.com>
2016-10-29 11:09:45 -0700

    Movie guide application with tableview

---
+++ MovieGuide/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  MovieGuide
+//
+//  Created by Chohee Kim on 10/28/16.
+//  Copyright © 2016 Chohee Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+    }
+
+
+
+}
+
