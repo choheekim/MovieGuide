431c90467078cb0934de03cb1b1f7400f53bd6c5
Chohee Kim <kch5559@gmail.com>
2016-11-06 15:44:46 -0800

    Updating .gitignore

---
+++ MovieGuide/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  MovieGuide
+//
+//  Created by Chohee Kim on 11/2/16.
+//  Copyright © 2016 Chohee Kim. All rights reserved.
+//
+
+import UIKit
+
+class Movie: NSObject {
+
+}
